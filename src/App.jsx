56dcdd677e3573cf2139309a354e1455ab22af6a
vuluu2k <vuluu040320@gmail.com>
2023-05-28 11:19:36 +0700

    convert vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.scss';
 import Lottie from 'react-lottie';
 
-import animationLove from 'assets/animations/mylove.json';
-import Music from 'components/Music';
-import Avatar from 'components/Avatar';
-import ImageLeft from 'assets/images/vuluu.jpg';
-import ImageRight from 'assets/images/bichboo.jpg';
+import animationLove from '@/assets/animations/mylove.json';
+import Music from '@/components/Music';
+import Avatar from '@/components/Avatar';
+import ImageLeft from '@/assets/images/vuluu.jpg';
+import ImageRight from '@/assets/images/bichboo.jpg';
 
 function App() {
   const defaultOptions = {
